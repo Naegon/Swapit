8ae8db2054207735b1861a43c2a7979959406aa4
Nathan <nathan111@free.fr>
2019-01-30 09:53:57 +0100

    script update utilisateur

--- Scripts_php/update_utilisateur.php
+++ Scripts_php/update_utilisateur.php
@@ -13,32 +13,143 @@ function update()
         return;
     }
 
-    try
-    {
-    $rep = $bdd->prepare('UPDATE compte_utilisateur SET variable = :variable WHERE nom = :nom AND prenom = :prenom');
+    $champ = $_GET['champ'];
 
-    $prenom = $_GET['prenom'];
-    $nom = $_GET['nom'];
-    $variable = $_GET['variable'];
+    if ($champ == "adresse_mail") {
+        try {
+            $rep = $bdd->prepare('UPDATE compte_utilisateur SET adresse_mail = :adresse_mail WHERE nom = :nom AND prenom = :prenom AND mdp = :mdp');
 
-    $rep->execute(array(
-        'variable' => $variable,
-        'nom' => $nom,
-        'prenom' => $prenom
-    ));
+            $prenom = $_GET['prenom'];
+            $nom = $_GET['nom'];
+            $mdp = $_GET['mdp'];
+            $adresse_mail = $_GET['texte'];
 
-    echo 'true';
+            $rep->execute(array(
+                'mdp' => $mdp,
+                'nom' => $nom,
+                'prenom' => $prenom,
+                'adresse_mail' => $adresse_mail
+            ));
 
-    $rep->closeCursor();
+            echo 'true';
 
+            $rep->closeCursor();
+
+        }
+        catch (PDOException $e) {
+            echo 'false';
+
+        }
     }
 
-    catch(PDOException $e)
+    else if ($champ == "biographie" )
     {
-        echo 'false';
+        try {
+            $rep = $bdd->prepare('UPDATE compte_utilisateur SET biographie = :biographie WHERE nom = :nom AND prenom = :prenom AND mdp = :mdp');
 
+            $prenom = $_GET['prenom'];
+            $nom = $_GET['nom'];
+            $mdp = $_GET['mdp'];
+            $biographie = $_GET['texte'];
+
+            $rep->execute(array(
+                'mdp' => $mdp,
+                'nom' => $nom,
+                'prenom' => $prenom,
+                'biographie' => $biographie
+            ));
+
+            echo 'true';
+
+            $rep->closeCursor();
+
+        }
+        catch (PDOException $e) {
+            echo 'false';
+
+        }
     }
+    else if ($champ == "promo")
+    {
+        try {
+            $rep = $bdd->prepare('UPDATE compte_utilisateur SET annee = :annee WHERE nom = :nom AND prenom = :prenom AND mdp = :mdp');
 
+            $prenom = $_GET['prenom'];
+            $nom = $_GET['nom'];
+            $mdp = $_GET['mdp'];
+            $annee = $_GET['texte'];
+
+            $rep->execute(array(
+                'mdp' => $mdp,
+                'nom' => $nom,
+                'prenom' => $prenom,
+                'annee' => $annee
+            ));
+
+            echo 'true';
+
+            $rep->closeCursor();
+
+        }
+        catch (PDOException $e) {
+            echo 'false';
+
+        }
+    }
+    else if ($champ == "numerotel")
+    {
+        try {
+            $rep = $bdd->prepare('UPDATE compte_utilisateur SET numerotel = :numerotel WHERE nom = :nom AND prenom = :prenom AND mdp = :mdp');
+
+            $prenom = $_GET['prenom'];
+            $nom = $_GET['nom'];
+            $mdp = $_GET['mdp'];
+            $numerotel = $_GET['texte'];
+
+            $rep->execute(array(
+                'mdp' => $mdp,
+                'nom' => $nom,
+                'prenom' => $prenom,
+                'numerotel' => $numerotel
+            ));
+
+            echo 'true';
+
+            $rep->closeCursor();
+
+        }
+        catch (PDOException $e) {
+            echo 'false';
+
+        }
+    }
+    else if ($champ == "section")
+    {
+        try {
+            $rep = $bdd->prepare('UPDATE compte_utilisateur SET section = :section WHERE nom = :nom AND prenom = :prenom AND mdp = :mdp');
+
+            $prenom = $_GET['prenom'];
+            $nom = $_GET['nom'];
+            $mdp = $_GET['mdp'];
+            $section = $_GET['texte'];
+
+            $rep->execute(array(
+                'mdp' => $mdp,
+                'nom' => $nom,
+                'prenom' => $prenom,
+                'section' => $section
+            ));
+
+            echo 'true';
+
+            $rep->closeCursor();
+
+        }
+        catch (PDOException $e) {
+            echo 'false';
+
+        }
+    }
 }
 
 update();
